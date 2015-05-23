34c155b4b57f423f9076e926294afd61b942d445
r3f1cul <nathanjolsen@gmail.com>
2015-05-23 13:22:29 -0700

    Added spring-boot

---
+++ src/main/java/com/r3f1cul/hello/Application.java
@@ -0,0 +1,14 @@
+package com.r3f1cul.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/r3f1cul/hello/HelloController.java
@@ -0,0 +1,29 @@
+package com.r3f1cul.hello;
+
+import org.apache.log4j.LogManager;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final org.apache.log4j.Logger LOGGER = LogManager.getLogger(HelloController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "/login/{username}/{password}", method = RequestMethod.GET)
+    public String login(@PathVariable String username, @PathVariable String password) {
+        LOGGER.info("Hellloooooooooo");
+        if (!StringUtils.isEmpty(username) && (!StringUtils.isEmpty(password))) {
+            return "Your usernmae is " + username + " and your password is " + password;
+        }
+        return "Someting Wong";
+    }
+
+}
