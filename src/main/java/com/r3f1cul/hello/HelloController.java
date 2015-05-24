f9f247c3945a7baf963540769f5d544d9d85c125
r3f1cul <nathanjolsen@gmail.com>
2015-05-23 17:04:04 -0700

    Added twitter bootstrap, removed jetty, using tomcat

--- src/main/java/com/r3f1cul/hello/HelloController.java
+++ src/main/java/com/r3f1cul/hello/HelloController.java
@@ -1,20 +1,21 @@
 package com.r3f1cul.hello;
 
 import org.apache.log4j.LogManager;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     private static final org.apache.log4j.Logger LOGGER = LogManager.getLogger(HelloController.class);
 
-    @RequestMapping("/")
+    @RequestMapping(value="/",  method=RequestMethod.GET)
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "login";
     }
 
     @RequestMapping(value = "/login/{username}/{password}", method = RequestMethod.GET)
